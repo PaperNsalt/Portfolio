789063be88c306a5f5d5529fec0d8e583b8e2b33
Jeremy O. Rellama <113158566+PaperNsalt@users.noreply.github.com>
2025-12-06 21:12:44 +0800

    Update styles in FirstSection and index files for improved layout and animations
    Update styles in FirstSection and index files for improved layout and animations

--- react-portfolio/src/FirstSection.jsx
+++ react-portfolio/src/FirstSection.jsx
@@ -4,16 +4,41 @@ function FirstSection(){
   return(
     <>
     <section>
-      <div className="grid grid-cols-2 py-4 px-4 border gap-2">
+      <div className="anim grid grid-cols-2 md:grid-rows-[80px] lg:grid-rows-[100px] xl:grid-rows-[190px] py-4 px-4 gap-2">
 
-      <div className='border flex justify-start items-center '>
-      <h1 className='text-sm md:text-base lg:text-lg xl:text-xl'>BEYOND <br/> DESIGN</h1>
+      <div className='anim flex justify-start items-center '>
+      <h1 className='text-left text-5x1 md:text-[10rem] lg:text-[14rem] xl:text-[24rem] leading-76 font-medium tracking-tighter'>BEYOND</h1>
+      </div>
       </div>
 
-      <div className='border'>
-      <h1>BEYOND <br/> DESIGN</h1>
+
+      <div className="anim grid grid-cols-2 md:grid-rows-[80px] lg:grid-rows-[100px] xl:grid-rows-[190px] py-4 px-4 gap-2">
+      <div className='anim flex justify-start items-center '>
+      <h1 className='text-left text-5x1 md:text-[10rem] lg:text-[14rem] xl:text-[24rem] leading-76 font-medium tracking-tighter'>DESIGN</h1>
       </div>
 
+      <div className='anim flex justify-center items-center'>
+      <h1 className='text-center text-4xl md:text-[1.4rem] border rounded-full w-auto p-2'>2025 - 2026</h1>
+      </div>
+      </div>
+
+
+      <div className="anim grid grid-cols-2 md:grid-rows-[80px] lg:grid-rows-[100px] xl:grid-rows-[190px] py-4 px-4 gap-2">
+
+      <div className='anim flex justify-center items-center'>
+      <h1 className='text-center text-4xl md:text-[1.4rem] border rounded-full w-auto p-2'>5 YEAR'S OF GRINDING</h1>
+      </div>
+
+      <div className='anim flex justify-end items-center '>
+      <h1 className='text-right text-5x1 md:text-[10rem] lg:text-[14rem] xl:text-[24rem] leading-76 font-medium tracking-tighter'>INTO</h1>
+      </div>
+      </div>
+
+      <div className="anim grid grid-cols-2 md:grid-rows-[80px] lg:grid-rows-[100px] xl:grid-rows-[190px] py-4 px-4 gap-2">
+
+      <div className='anim col-start-2 col-end-3 flex justify-end items-center '>
+      <h1 className=' text-left text-5x1 md:text-[10rem] lg:text-[14rem] xl:text-[24rem] leading-76 font-medium tracking-tighter'>EXPERIENCE</h1>
+      </div>
       </div>
     </section>
     </>

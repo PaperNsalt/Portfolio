b1381a898a5901d14a9e9cbbfb496390e393f89c
Jeremy O. Rellama <113158566+PaperNsalt@users.noreply.github.com>
2025-12-06 15:50:10 +0800

    Add FirstSection component and update layout styles

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,13 +1,16 @@
 import Header from './Header'
 import Footer from './Footer'
 import './App.css'
+import FirstSection from './FirstSection'
 
 function App() {
 
   return (
     <>
   <Header></Header>
-<FirstSection></FirstSection>
+
+  <FirstSection></FirstSection>
+  
   <Footer></Footer>
     </>
   )
---
+++ react-portfolio/src/FirstSection.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+function FirstSection(){
+
+  return(
+    <>
+    <section>
+      <div className="grid grid-cols-2 py-4 px-4 border gap-2">
+
+      <div className='border flex justify-start items-center '>
+      <h1 className='text-sm md:text-base lg:text-lg xl:text-xl'>BEYOND <br/> DESIGN</h1>
+      </div>
+
+      <div className='border'>
+      <h1>BEYOND <br/> DESIGN</h1>
+      </div>
+
+      </div>
+    </section>
+    </>
+  );
+}
+
+export default FirstSection
